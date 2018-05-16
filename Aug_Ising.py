0ed318a7538cd2e4d246932e897cd6bb6129d54f
kaywu <kaywu@localhost.localdomain>
2018-05-16 09:50:30 +0800

    modify class structure and fix the augumented bug

--- Aug_Ising.py
+++ Aug_Ising.py
@@ -1,118 +1,144 @@
 import numpy as np
 
 
+#def Normal_Distro_Initializer(x,mean,std):
+#    return np.random.normal(mean,std,size=len(x))
+
+
+
+
+
+def Update_HMC(obj,mass,N_LeapFrog,LFSize):
+
+    ## K(p) = p**2/(2*mom_std)
+    ## U(y) = -y**2/(2)
+    
+    ## renew momentum wrt gaussian distro.
+    std_p = 2*mass
+    new_p = np.random.normal(0,std_p,size=obj.N)
+    
+    ## Couple with potiential (Ising model) to evolve y
+    ## Leap Frog: 
+    new_y = obj.y
+    for l in range(N_LeapFrog):
+        new_p = new_p - LFSize*0.5* (-new_y)
+        new_y = new_y + LFSize*new_p/mass
+        new_p = new_p - LFSize*0.5*(-new_y)
+
+    ## M-H :
+    dE = np.sum(new_p**2-obj.p**2)/std_p + obj.fx_GetU(new_y) - obj.fx_GetU(obj.y)
+    print(new_p)
+    #print(p_new)    
+    A = np.exp(-dE/obj.T)
+    if A >= 1:
+        obj.y = new_y
+        obj.x = np.sign(obj.y)
+        obj.p = new_p
+    elif np.random.uniform() < A:
+        obj.y = new_y
+        obj.x = np.sign(obj.y)
+        obj.p = new_p
+
+
+def Update_SSU(obj):
+    ## rand loc <x,y>
+    xy = np.random.randint(obj.L,size=2)
+
+    ## Get neigh.idxs
+    neigh = obj.where_allneigh(xy[0],xy[1]) ## y => 1,x => 0
+
+    ## Get neigh confg
+    h =  np.sum(obj.x[neigh])
+
+    ## proposal :
+    new_y = np.random.uniform(-1,1)
+
+    ## calculate dE:
+    st = xy[1]*obj.L + xy[0]
+    dE =  (-np.sign(new_y) + obj.x[st])*h - (obj.y[st]**2 - new_y**2)/2
+
+    ## Metropolis
+    A = np.exp(-dE/obj.T) 
+    if A >= 1:
+        obj.y[st] = new_y
+        obj.x[st] = np.sign(new_y)
+    elif np.random.uniform() < A:
+        obj.y[st] = new_y
+        obj.x[st] = np.sign(new_y)
+
+
+
 
 class Aug_Ising2D_CMC:
-    def __init__(self,L,T,is_hmc=False,mom_std=1.0,LF_step=0.06):
+    def __init__(self,L,T):
         self.L = L
         self.N = L**2
-        self.y = np.random.uniform(-1.,1.,size=self.N)
-        self.x = np.sign(self.y)
         self.T = T
+        ## augumented :
+        self.y = np.ones(self.N)
 
+        ## Ising config.:
+        self.x = np.sign(self.y)
+
+        ## momentum :
+        self.p = np.zeros(self.N)
+        
+    
+        ## This is for measurement:
         self.Typ = {'M':0,'M2':1,'M4':2,'E':3}
         self.Obs = np.zeros(len(self.Typ))
         self.MCS_cntr = 0
-        self.is_hmc=is_hmc
-        self.p = None
-        self.mom_std = mom_std
-        self.LF_step = LF_step
-        if self.is_hmc:
-            self.p = np.random.normal(0,2*self.mom_std,size=self.N)
+
+
+        #if self.is_hmc:
+        #    self.p = np.random.normal(0,2*self.mom_std,size=self.N)
         
-
-
-    def reset(self):
-        self.y = np.random.uniform(-1.,1.,size=self.L**2)
+    def Reset(self):
+        self.y = np.ones(self.N)
         self.x = np.sign(self.y)
+        self.p = np.zeros(self.N)
         self.Clear_Measurement()
-
-    def Evaluate(self,y2,weight=1.):
-        E2 = GetE(y2)
-        E  = GetE(self.y)
-
-        A = np.exp(-(E2-E)/self.T) * weight
-        if A >= 1 :
-            return 1
-        elif np.random.uniform() < A:
-            return 1
-        else :
-            return 0
-
-    def GetE(self,y2):
-        """
-            This is a private function
-            Do not call directly.
-        """
-        Ag = -np.dot(y2,y2)*0.5
-
-        tmp = np.sign(y2).reshape((self.L,self.L))
-        E = -np.sum(tmp * (np.roll(tmp,1,axis=1) + np.roll(tmp,1,axis=0)))
-        return Ag + E
-
-    def Update(self,y2):
-        self.y = np.copy(y2)
-
-
-    def Algo_SSU(self,weight=1.):
-        xy = np.random.randint(self.L,size=2)
-        neigh = np.array([xy[0]*self.L + (xy[1] + 1)%self.L,\
-                        xy[0]*self.L + (xy[1]-1+self.L)%self.L,\
-                        ((xy[0]+1)%self.L)*self.L + xy[1],\
-                        ((xy[0]-1+self.L)%self.L)*self.L + xy[1]])
-        h =  np.sum(self.x[neigh])\
-
-        st = xy[0]*self.L + xy[1]
-        new_y = np.random.uniform(-1,1)
-
-        dE =  (-np.sign(new_y) + self.x[st])*h + (self.y[st]**2 - new_y**2)/2
-
-        A = np.exp(-dE/self.T) * weight
-        if A >= 1:
-            self.y[st] = new_y
-            self.x[st] = np.sign(new_y)
-        elif np.random.uniform() < A:
-            self.y[st] = new_y
-            self.x[st] = np.sign(new_y)
-
-    def Algo_HMC(self):
-        if not self.is_hmc:
-            print("[ERROR] the hmc is not enabled")
-            exit(99)
-        ## K(p) = p**2/(2*mom_std)
-        ## U(y) = -y**2/(2)
-        ## renew momentum wrt gaussian distro.
-        p_s = np.random.normal(0,2*self.mom_std,size=self.N)
         
-        ## Couple with potiential (Ising model) to evolve y
-        ## Leap Frog: 
-        p_new = p_s - self.LF_step*0.5* (-self.y)
-        s_new = self.y + self.LF_step*p_new/self.mom_std
-        p_new = p_new - self.LF_step*0.5*(-s_new)
 
-        ## M-H :
-        dE = np.sum(p_new**2-self.p**2)/(2.*self.mom_std) + self.GetE(s_new) - self.GetE(self.y)
-        #print(p_new)    
-        A = np.exp(-dE/self.T)
-        if A >= 1:
-            self.y = s_new
-            self.x = np.sign(self.y)
-            self.p = p_new
-        elif np.random.uniform() < A:
-            self.y = s_new
-            self.x = np.sign(self.y)
-            self.p = p_new
+    def where_allneigh(self,x,y):
+        return  np.array([y*self.L + (x + 1)%self.L,\
+                          y*self.L + (x-1+self.L)%self.L,\
+                          ((y+1)%self.L)*self.L + x,\
+                          ((y-1+self.L)%self.L)*self.L + x])
+
+    def allneigh_x(self,x,y):
+        return self.x[self.where_allneigh(x,y)]
+    
+    def allneigh_y(self,x,y):
+        return self.y[self.where_allneigh(x,y)]
+        
+    def fx_GetU_ising(self,y2):
+        tmp = np.sign(y2).reshape((self.L,self.L))
+        return -np.sum(tmp * (np.roll(tmp,1,axis=1) + np.roll(tmp,1,axis=0)))
+    
+    def fx_GetU(self,y2):
+        Ag = np.dot(y2,y2)*0.5
+
+        return Ag + self.fx_GetU_ising(y2)
+    
+    def fx_GetK(self,p2,mass):
+        Ag = np.dot(p2,p2)/(2.*mass)
+        return Ag    
+    
+    def fx_GetE(self,p2,mass,y2):
+        return self.fx_GetK(p2,mass) + self.fx_GetU(y2)
 
 
     def Measurement(self,weight=1.):
-        M2 = np.sum(self.x)/self.L**2
+        M2 = np.sum(self.x)/self.N
         M2 = M2**2
 
         self.Obs[self.Typ['M']]  += np.sqrt(M2)
         self.Obs[self.Typ['M2']] += M2
         self.Obs[self.Typ['M4']] += M2**2
-        self.Obs[self.Typ['E']] += self.GetE(self.x) / self.L**2
+        self.Obs[self.Typ['E']] += self.fx_GetU_ising(self.y) / self.N
         self.MCS_cntr += 1
+        
     def Statistic(self):
         self.Obs /= self.MCS_cntr
 
@@ -122,28 +148,33 @@ class Aug_Ising2D_CMC:
 
 if __name__ == "__main__":
 
-    L = 8
+    L = 2
     T = 10.0
     EQUIN = 40000
     BINNUM = 10
     BINSZ  = 20000
+    
+    mass      = 2
+    NLeapFrog = 10
+    LeapStepSize = 0.04
+    
 
     ##MC = Aug_Ising2D_CMC(L,T,is_hmc=1)
-    MC = Aug_Ising2D_CMC(L,T,is_hmc=1)
+    MC = Aug_Ising2D_CMC(L,T)
     
     #print ( MC.GetE(MC.x) )
     BinData = []
     print ("Equi")
     for i in range(EQUIN):
-        #MC.Algo_SSU()
-        MC.Algo_HMC()
+        Update_HMC(MC,mass,NLeapFrog,LeapStepSize)
+        #Update_SSU(MC)
 
     print ("statistic")
     for b in range(BINNUM):
         print ("Bin %d"%(b))
         for sz in range(BINSZ):
-            #MC.Algo_SSU()
-            MC.Algo_HMC()
+            Update_HMC(MC,mass,NLeapFrog,LeapStepSize)
+            #Update_SSU(MC)
             MC.Measurement()
         MC.Statistic()
         # get Bin data:
